356920c7bb999b56381ed6c5ddb3eddc4e722b69
wujun0919 <wujun0919@qq.com>
2019-09-09 16:10:16 +0800

    0.0.1

---
+++ mpApp/pages/test/test.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<view class="icon-icon_User iconfont animated fadeInDownBig" ></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
